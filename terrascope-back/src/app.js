9f480b09ddf2d2926cc637c4d649c470e3caaf7a
RonaldoChavez76 <chavezronaldo675@gmail.com>
2025-10-15 12:14:01 -0600

    FEAT-IdentificadorIA
    Se creo un controlador y se instalo la extensión de Google IA para la identificación de animales por imagen

--- terrascope-back/src/app.js
+++ terrascope-back/src/app.js
@@ -5,6 +5,7 @@ import "./config/db.js"; // Conectar a la base de datos
 import habitatRoutes from "./routes/habitat.routes.js";
 import floraFaunaRoutes from "./routes/Fauna_Flora.routes.js";
 import usuarioRoutes from "./routes/usuario.routes.js";
+import iaRoutes from "./routes/ia.routes.js";
 
 
 const app = express();
@@ -13,12 +14,14 @@ const app = express();
 // Middlewares
 app.use(cors());
 app.use(morgan("dev"));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // Rutas
 app.use("/api/habitats", habitatRoutes);
 app.use("/api/usuarios", usuarioRoutes);
 app.use("/api/fauna-flora", floraFaunaRoutes );
+app.use("/api/ia", iaRoutes);
 
 
 // Manejo de errores
